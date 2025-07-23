d01a960749400d30d57b2ceebf2d0c922ef62649
Mateejr <vallian476@gmail.com>
2025-07-23 11:26:20 +0700

    update time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             <PageSwitcher />
             {/* Watermark version info */}
             <div className="fixed bottom-1 left-1/2 -translate-x-1/2 text-[10px] md:text-xs text-gray-500 opacity-70 pointer-events-none select-none z-50 md:top-0.5 md:bottom-auto">
-              Versi: v.0.5.3 · Terakhir Update: 17 Juli
+              Versi: v.0.6.0 · Terakhir Update: 23 Juli
             </div>
             <main className="mt-14 md:mt-0 md:ml-20">
               {children}
